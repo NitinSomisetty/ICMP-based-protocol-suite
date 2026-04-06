3d3cc1f3cfb62b81b34de1ee6568c456f0112deb
Nitin Somisetty <nitin.somisetty@gmail.com>
2026-04-06 12:19:48 +0530

    changes to ssl

--- ssl_utils.py
+++ ssl_utils.py
@@ -3,14 +3,15 @@ import ssl
 # socket -> ssl.wrap -> encrypted communication
 
 def create_server_ssl_context(): # for server side, we need to load the certificate and private key
-    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH) 
+#An SSL context object is a Python object that stores all TLS/SSL settings for secure connections.
     context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
     context.minimum_version = ssl.TLSVersion.TLSv1_2
     return context
  # So traffic is encrypted, but the client does not authenticate the server identity
 
 def create_client_ssl_context(): 
-    context = ssl.create_default_context()
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+    context.verify_mode = ssl.CERT_NONE # accepts any certificate, even if it's self-signed or invalid.
     return context
